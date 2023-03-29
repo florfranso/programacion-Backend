86db46426f943721f4b3c75d54759400eab43edc
flor perez <flor.franso@live.com>
2023-03-29 18:53:56 -0300

    bae profiling-log-debug

---
+++ Desafio 16 - gzip-logs-progiling-debug/src/apis/routerRandom.js
@@ -0,0 +1,21 @@
+import express, { query } from 'express';
+const {Router} = express
+import {fork} from 'child_process';
+
+const randomRouter = new Router()
+const forkedProcess = fork('')
+
+randomRouter.use(express.json())
+randomRouter.use(express.urlencoded({extended: true}))
+
+randomRouter.get('/random', (req, res) => {
+    let numero = req.query
+    forkedProcess.send(parseInt(numero.valor));
+    forkedProcess.on('message', stringified => {
+        console.log(stringified)
+    });
+    res.send('Calculando numeros random en forked process')
+    
+})
+
+export default randomRouter;
